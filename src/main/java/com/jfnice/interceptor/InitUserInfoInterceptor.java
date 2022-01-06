58ebff9a0938c7f838f756691a0c2ae5b1ee7bf8
konglm <klm.821222@163.com>
2022-01-06 11:56:11 +0800

    同步学生前先判断是否有班级

--- src/main/java/com/jfnice/interceptor/InitUserInfoInterceptor.java
+++ src/main/java/com/jfnice/interceptor/InitUserInfoInterceptor.java
@@ -8,6 +8,7 @@ import com.jfinal.kit.StrKit;
 import com.jfnice.annotation.JsyPermissions;
 import com.jfnice.annotation.ShiroClear;
 import com.jfnice.core.JFniceBaseController;
+import com.jfnice.enums.FinishEnum;
 import com.jfnice.enums.Logical;
 import com.jfnice.enums.OpCodeEnum;
 import com.jfnice.enums.ResultEnum;
@@ -15,8 +16,13 @@ import com.jfnice.ext.CondPara;
 import com.jfnice.ext.CurrentUser;
 import com.jfnice.ext.ErrorMsg;
 import com.school.api.gx.PtApi;
+import com.school.api.gx.RsApi;
+import com.school.api.model.Cls;
 import com.school.library.userinfo.UserInfoLogic;
 import com.school.library.userinfo.UserTypeEnum;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 初始化用户信息拦截器，每次进入系统，判断用户信息是否为空，为空则去同步信息
@@ -28,9 +34,11 @@ public class InitUserInfoInterceptor implements Interceptor {
     @Override
     public void intercept(Invocation inv) {
         try {
-            //判断学生信息是否为空，为空则进行同步操作
+            //判断学生信息是否为空，为空且已经有班级信息则进行同步操作
             boolean existsStu = userLogic.existsByType(CurrentUser.getSchoolCode(), UserTypeEnum.STUDENT.getUserType());
-            if(!existsStu){
+            List<Cls> clsList = RsApi.getClsList(FinishEnum.ALL.getK());
+            List<String> clsCodeList = clsList.stream().map(c -> c.getClsCode()).collect(Collectors.toList());
+            if(!existsStu && (clsCodeList.size() != 0)){
                 userLogic.synchronizeStuInfo();
             }
 
